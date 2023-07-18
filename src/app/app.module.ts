932373a5aefe7679357e91b226612163da153377
monaSaurusRex <moniquenagel24.mn@gmail.com>
2023-07-18 20:45:55 +0200

    Added font awesome icons to project files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FontAwesomeModule, FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import { faCalendar, faPlus, faTable } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
     declarations: [AppComponent, NavbarComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
     providers: [],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(iconLib: FaIconLibrary){
+        iconLib.addIcons(faPlus, faCalendar, faTable);
+    }
+}
