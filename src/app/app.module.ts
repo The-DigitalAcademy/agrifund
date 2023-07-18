c2dac98524b9e34f8954f821507e5b0b25a5fdcd
Bolebo67 <bolebo7@gmail.com>
2023-07-18 08:46:06 +0200

    I added a page for a landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 @NgModule({
-    declarations: [AppComponent, NavbarComponent, LandingComponent],
+    declarations: [AppComponent, NavbarComponent, LandingComponent, LandingPageComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
