06b7327722a1a780c99859af486a095fbd95a3d0
Bolebo67 <bolebo7@gmail.com>
2023-07-18 15:41:11 +0200

    I added about company

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { TeamSectionComponent } from './team-section/team-section.component';
+import { TeamSectionComponent } from './components/team-section/team-section.component';
+import { AboutCompanyComponent } from './components/about-company/about-company.component';
 
 @NgModule({
-    declarations: [AppComponent, NavbarComponent, LandingPageComponent, LandingPageComponent, TeamSectionComponent],
+    declarations: [AppComponent, NavbarComponent, LandingPageComponent, LandingPageComponent, TeamSectionComponent, AboutCompanyComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
