b727dc9016ebb8b92a8c71cfafbeb134e864dcb4
Bolebo67 <bolebo7@gmail.com>
2023-07-18 08:59:28 +0200

    I added app routingcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LandingComponent } from './components/landing/landing.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 @NgModule({
-    declarations: [AppComponent, NavbarComponent, LandingComponent, LandingPageComponent],
+    declarations: [AppComponent, NavbarComponent, LandingPageComponent, LandingPageComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
