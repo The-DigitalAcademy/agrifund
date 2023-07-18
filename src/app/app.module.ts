5248b7d4089638bd08d11c7c20ed27826b7b400e
ntokozoblessingradebe98@gmail.com <ntokozoblessingradebe98@gmail.com>
2023-07-18 11:19:51 +0200

    added  ontainers in the register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RegisterComponent } from './pages/register/register.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
     declarations: [AppComponent, NavbarComponent, RegisterComponent],
     imports: [BrowserModule, AppRoutingModule],
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+name = new  FormControl('');
 }
