ac9bd2674f0bfcaaa769dfcc433dd291497767cf
Kamogelo0404 <kamogelonkadimeng0404@gmail.com>
2023-07-19 12:20:23 +0200

    added the form for crop info

--- src/app/components/disabledform-crop-info/disabledform-crop-info.component.ts
+++ src/app/components/disabledform-crop-info/disabledform-crop-info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-disabledform-crop-info',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./disabledform-crop-info.component.css']
 })
 export class DisabledformCropInfoComponent {
+
+  myForm!: FormGroup;
+  isDisabled: boolean = true;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    const userData = {
+      seasonFarm: 'Spring',
+      crops: ['Corn', 'Tomatoes'],
+      seedAmount: '50'
+    };
+  
+    this.myForm = this.fb.group({
+      seasonFarm: new FormControl({ value: userData.seasonFarm, disabled: true }),
+      crops: new FormControl({ value: userData.crops, disabled: true }),
+      seedAmount: new FormControl({ value: userData.seedAmount, disabled: true })
+    });
+  }
+
+  enableFields() {
+    this.isDisabled = false;
+    this.myForm.enable();
+  }
 
 }
