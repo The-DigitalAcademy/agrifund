1bcacdfcb36cc11c8b2f59e38dd43a9bee8a36d6
Kamogelo0404 <kamogelonkadimeng0404@gmail.com>
2023-07-19 12:26:58 +0200

    added the dropdown of the seasons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DisabledformPersonalInfoComponent } from './components/feature-disabledform-personal-info/feature-disabledform-personal-info.component';
 import { PersonalInfoFormComponent } from './components/personal-info-form/personal-info-form.component';
+import { DisabledformCropInfoComponent } from './components/disabledform-crop-info/disabledform-crop-info.component';
 
 
 const routes: Routes = [
-    {path: '',component: PersonalInfoFormComponent}
+    {path: '',component: PersonalInfoFormComponent},
+    {path: 'crop',component: DisabledformCropInfoComponent}
 ];
 
 @NgModule({
