04c46a08c28ad78157a06b1600c21cf2a3f0de0e
Kamogelo0404 <kamogelonkadimeng0404@gmail.com>
2023-07-19 11:44:06 +0200

    added the form to routr to prsonal info-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DisabledformPersonalInfoComponent } from './components/feature-disabledform-personal-info/feature-disabledform-personal-info.component';
+import { PersonalInfoFormComponent } from './components/personal-info-form/personal-info-form.component';
 
 
 const routes: Routes = [
-    {path: '',component: DisabledformPersonalInfoComponent}
+    {path: '',component: PersonalInfoFormComponent}
 ];
 
 @NgModule({
