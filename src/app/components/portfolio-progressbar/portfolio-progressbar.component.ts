c5b906b9a8c9d84183a24d899abb4789a042a827
Kamogelo0404 <kamogelonkadimeng0404@gmail.com>
2023-07-20 10:46:31 +0200

    styled the portfolio page

---
+++ src/app/components/portfolio-progressbar/portfolio-progressbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-progressbar',
+  templateUrl: './portfolio-progressbar.component.html',
+  styleUrls: ['./portfolio-progressbar.component.css']
+})
+export class PortfolioProgressbarComponent {
+
+}
