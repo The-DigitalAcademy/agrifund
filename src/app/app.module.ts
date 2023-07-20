3cda7ab2bbe79b42c931533bd954a5d9a3265280
Monique <monqiquenagel24.mn@gmail.com>
2023-07-20 09:15:29 +0200

    Created bookkeep view all page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BookkeepViewAllComponent } from './pages/bookkeep-view-all/bookkeep-view-all.component';
 
 @NgModule({
-    declarations: [AppComponent, NavbarComponent],
+    declarations: [AppComponent, NavbarComponent, BookkeepViewAllComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
---
+++ src/app/pages/bookkeep-view-all/bookkeep-view-all.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookkeep-view-all',
+  templateUrl: './bookkeep-view-all.component.html',
+  styleUrls: ['./bookkeep-view-all.component.css']
+})
+export class BookkeepViewAllComponent {
+
+}
