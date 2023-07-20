19e63f28dd0bc87bddd953fe688c58b96014d2aa
ntokozoblessingradebe98@gmail.com <ntokozoblessingradebe98@gmail.com>
2023-07-20 10:49:02 +0200

    added field to forgot password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
 const routes: Routes = [
     {path:'login',component:LoginComponent},
-    {path:'register',component:RegisterComponent}
+    {path:'register',component:RegisterComponent},
+    {path:'forgot-password',component:ForgotPasswordComponent}
 ];
 
 @NgModule({
