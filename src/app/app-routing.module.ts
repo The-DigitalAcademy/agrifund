727da222774669ea5144f3f8054a4fcaea9a8029
monaSaurusRex <moniquenagel24.mn@gmail.com>
2023-07-20 20:59:40 +0200

    Made landing page default page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 const routes: Routes = [
-    {path:'login',component:LoginComponent},
-    {path:'register',component:RegisterComponent},
-    {path:'forgot-password',component:ForgotPasswordComponent},
-    {path:'reset-password',component:ResetPasswordComponent},
-   
+    { path: '', component: LandingPageComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent },
+    { path: 'reset-password', component: ResetPasswordComponent },
 ];
 
 @NgModule({
