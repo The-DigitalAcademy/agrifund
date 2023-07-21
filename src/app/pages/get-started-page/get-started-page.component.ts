be52f4ca4d91784fc98e64e94797d57b525c8044
Mamokgosi <preciousmaphutha@173@gmail.com>
2023-07-21 12:06:12 +0200

    Fixed the buttons and the router link

---
+++ src/app/pages/get-started-page/get-started-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-started-page',
+  templateUrl: './get-started-page.component.html',
+  styleUrls: ['./get-started-page.component.css']
+})
+export class GetStartedPageComponent {
+
+}
