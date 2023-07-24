9f8f5d303030633adcf365caf6f0e9343998dcd3
Kamogelo0404 <kamogelonkadimeng0404@gmail.com>
2023-07-24 10:31:18 +0200

    added disabledform for farm info

---
+++ src/app/components/disabledform-farm-info/disabledform-farm-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-disabledform-farm-info',
+  templateUrl: './disabledform-farm-info.component.html',
+  styleUrls: ['./disabledform-farm-info.component.css']
+})
+export class DisabledformFarmInfoComponent {
+
+}
