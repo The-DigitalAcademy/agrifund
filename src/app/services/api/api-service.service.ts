c6fa2b8a3ad999592714f7a5ff2936c5820e8b6f
Monique <monqiquenagel24.mn@gmail.com>
2023-08-01 16:18:18 +0200

    added methods to get and pass data to the mock api.

--- src/app/services/api/api-service.service.ts
+++ src/app/services/api/api-service.service.ts
@@ -1,9 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ApiServiceService {
+    constructor(private http: HttpClient) {}
 
-  constructor() { }
+    // base string for api connection
+    private baseUrl = 'localhost:3001/agrifund';
+
+    /* --------------------------------
+        BOOKKEEP / INCOME STATEMENT
+    ---------------------------------*/
+
+    // get income statement data based on the farm id
+    getIncomeStatements(farmId: number) {
+        return this.http.get(`${this.baseUrl}/income-statement/${farmId}`);
+    }
+
+    // getIncomeStement by their id
+    getIncomeStament(statmentId: number) {
+        return this.http.get(`${this.baseUrl}/income-statement/${statmentId}`);
+    }
+
+    // get all bookkeep records based on the statement id
+    getStatementItems(statementId: number) {
+        return this.http.get(`${this.baseUrl}/income-statement/${statementId}`);
+    }
+
+    // get a single income statement item
+    getStamentItem(recordId: number) {
+        return this.http.get(`${this.baseUrl}/income-statement/${recordId}`);
+    }
+
+    // add a new income statement record
+    addRecord(body: any) {
+        return this.http.post(`${this.baseUrl}/income-statement`, body);
+    }
+
+    // update data for a single income statement record
+    updateRecord(recordId: number, body: any) {
+        return this.http.put(
+            `${this.baseUrl}/income-statement/${recordId}`,
+            body
+        );
+    }
+
+    // get income statement records between two dates
+
+    //get an income statement record from a search text
 }
