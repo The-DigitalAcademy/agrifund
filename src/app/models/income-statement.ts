5fd096b3b831b40edfd794203e7ec648151e244c
Monique <monqiquenagel24.mn@gmail.com>
2023-08-01 15:15:38 +0200

    Changed date data type to string.

--- src/app/models/income-statement.ts
+++ src/app/models/income-statement.ts
@@ -1,8 +1,7 @@
 export interface IncomeStatement {
     farm_id: number;
-    statement_date: number;
+    statement_date: string;
     total_income: number;
     total_expenses: number;
     net_income: number;
-
 }
