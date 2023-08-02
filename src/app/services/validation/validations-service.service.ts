87155878d025781d6a2a8334dee8142b40846e04
Mamokgosi <preciousmaphutha@173@gmail.com>
2023-08-02 08:50:37 +0200

    I have added the method for date format in the validations service

--- src/app/services/validation/validations-service.service.ts
+++ src/app/services/validation/validations-service.service.ts
@@ -9,7 +9,6 @@ export class ValidationsServiceService {
 
   //Method to validates password
   validatePassword(password: string): boolean {
-    // This is the method to implement the  password strength validation.
     // We are checking if the password meets minimum requirements such as length, uppercase, lowercase, digits, special characters)
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return passwordRegex.test(password);
@@ -17,11 +16,14 @@ export class ValidationsServiceService {
 
   //Method to validates email address
   validateEmail(email: string): boolean {
-    // Use regular expression to validate the email format
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
   
   //Method to validate date
+  validateDateFormat(date: string): boolean {
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    return dateRegex.test(date);
+  }
   
 }
