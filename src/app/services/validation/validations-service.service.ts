36efd8ecce2ba5d1488e0b9d8bde87d7f236e1cf
Mamokgosi <preciousmaphutha@173@gmail.com>
2023-08-02 08:48:27 +0200

    I have added the method for email and password in the validations service

--- src/app/services/validation/validations-service.service.ts
+++ src/app/services/validation/validations-service.service.ts
@@ -6,4 +6,22 @@ import { Injectable } from '@angular/core';
 export class ValidationsServiceService {
 
   constructor() { }
+
+  //Method to validates password
+  validatePassword(password: string): boolean {
+    // This is the method to implement the  password strength validation.
+    // We are checking if the password meets minimum requirements such as length, uppercase, lowercase, digits, special characters)
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+  }
+
+  //Method to validates email address
+  validateEmail(email: string): boolean {
+    // Use regular expression to validate the email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
+  //Method to validate date
+  
 }
