edaddff8f04624d2d3789daa1b25530489b9d741
Mamokgosi <preciousmaphutha@173@gmail.com>
2023-08-02 09:02:28 +0200

    I have added the method for field to only accept numbers not text

--- src/app/services/validation/validations-service.service.ts
+++ src/app/services/validation/validations-service.service.ts
@@ -21,9 +21,30 @@ export class ValidationsServiceService {
   }
   
   //Method to validate date
+  //It checks if the date is in the format of 'YYYY/MM/DD'.
   validateDateFormat(date: string): boolean {
     const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
     return dateRegex.test(date);
   }
   
+  //Method to validate numbers
+   // The method below to checks if a number is positive.
+   validatePositiveNumber(value: number): boolean {
+    return value > 0;
+  }
+
+    //This method below only accepts numbers
+   isNumber(value: any): boolean {
+    if (typeof value === 'number') {
+      // Check if the value is a valid number (not NaN)
+      return !isNaN(value);
+    } else if (typeof value === 'string') {
+      // Convert the string to a number and check if it's a valid number (not NaN)
+      const numericValue = Number(value);
+      return !isNaN(numericValue);
+    } else {
+      // Not a number or a string, so it's not valid
+      return false;
+    }
+  }
 }
