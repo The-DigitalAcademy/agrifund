3bb99e1793ff86f77bb78f374f39a116d7dba540
Monique <monqiquenagel24.mn@gmail.com>
2023-08-03 12:25:02 +0200

    Capturing data from create form and printing it in the console.

--- src/app/pages/bookkeep-create/bookkeep-create.component.spec.ts
+++ src/app/pages/bookkeep-create/bookkeep-create.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookkeepCreateComponent } from './bookkeep-create.component';
 
 describe('BookkeepCreateComponent', () => {
-  let component: BookkeepCreateComponent;
-  let fixture: ComponentFixture<BookkeepCreateComponent>;
+    let component: BookkeepCreateComponent;
+    let fixture: ComponentFixture<BookkeepCreateComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BookkeepCreateComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [BookkeepCreateComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(BookkeepCreateComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(BookkeepCreateComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
