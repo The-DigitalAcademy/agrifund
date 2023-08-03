aa9ceb4578f3105dc4d8eaf01e035a419bb57d44
Mamokgosi <preciousmaphutha@173@gmail.com>
2023-08-03 09:25:21 +0200

    I have added the validations on the register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -8,12 +8,13 @@ import { ValidationsServiceService } from 'src/app/services/validation/validatio
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit{ 
+  RegisterForm!: FormGroup; 
 
 constructor(private fb:FormBuilder, private validationsService: ValidationsServiceService) {}
 
 ngOnInit(): void {
 
-  RegisterForm = new FormGroup({
+  this.RegisterForm = this.fb.group({
 
     name: new FormControl({value: '', disabled:true }, [Validators.required, this.validationsService.textWithoutNumbersValidator()]),
     surname: new FormControl({value: '', disabled:true }, [Validators.required, this.validationsService.textWithoutNumbersValidator()]),
@@ -21,49 +22,19 @@ ngOnInit(): void {
     cellNo: new FormControl({value: '', disabled:true }, [Validators.required, this.validationsService.phoneNumberValidator()]),
     password:new FormControl({value: '', disabled:true }, [Validators.required, this.validationsService.passwordValidator()]),
     Cpassword: new FormControl({value: '', disabled:true },)
-    })
+    },{
+            validators: [this.validationsService.passwordsMatchValidator] // Add custom password matching validation method from your validation service
+          });
     }
-    
-}
-
-// import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { ValidationsServiceService } from 'src/app/services/validation/validations-service.service';
-
-// @Component({
-//   selector: 'app-register',
-//   templateUrl: './register.component.html',
-//   styleUrls: ['./register.component.css']
-// })
-// export class RegisterComponent implements OnInit {
-//   RegisterForm!: FormGroup;
-
-//   constructor(
-//     private formBuilder: FormBuilder,
-//     private validationService: ValidationsServiceService
-//   ) { }
-
-//   ngOnInit() {
-//     this.RegisterForm = this.formBuilder.group({
-//       name: ['', Validators.required],
-//       surname: ['', Validators.required],
-//       id: ['', Validators.required, this.validationService.validateIDno],
-//       cellNo: ['', Validators.required, this.validationService.validatePhoneNumber], // Add custom cell number validation method from your validation service
-//       password: ['', [Validators.required, Validators.minLength(8)]],
-//       Cpassword: ['', Validators.required],
-//     }, {
-//       validators: [this.validationService.validatePassword] // Add custom password matching validation method from your validation service
-//     });
-//   }
-
-//   // Add the method to handle form submission
-//   onSubmit() {
-//     if (this.RegisterForm.valid) {
-//       // Process the form data here (e.g., call a service to register the user)
-//     } else {
-//       // Display an error message or handle invalid form submission
-//     }
-//   }
-// }
+    onSubmit() {
+          if (this.RegisterForm.valid) {
+            // Process the form data here (e.g., call a service to register the user)
+          } else {
+            // Display an error message or handle invalid form submission
+          }
+        }
+      }
+  
+  
 
                               
