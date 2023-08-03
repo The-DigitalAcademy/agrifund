12d58fb519292e513fe40bce43bc3720ce2effdf
Monique <monqiquenagel24.mn@gmail.com>
2023-08-03 09:10:22 +0200

    Renamed bookkeep service.

---
+++ src/app/services/bookkeep/bookkeep.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { IncomeStatementItem } from 'src/app/models/income-statement-item';
+import { ApiService } from '../api/api.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class BookkeepService {
+    constructor() {}
+
+    // TODO
+    // update income statement total income
+
+    // TODO
+    // update income statement total expense
+
+    // TODO
+    // update income statement total net income (profit)
+}
