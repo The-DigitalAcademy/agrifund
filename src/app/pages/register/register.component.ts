f40ca314ae63901a8e41873e9b5aa224f21e7a99
Mamokgosi <preciousmaphutha@173@gmail.com>
2023-08-03 12:22:37 +0200

    password validation

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -21,7 +21,7 @@ ngOnInit(): void {
     id_number: new FormControl("",[Validators.required, this.validationsService.idNumberValidator()]),
     cell_number: new FormControl( "",[Validators.required, this.validationsService.phoneNumberValidator()]),
     password:new FormControl("", [Validators.required, this.validationsService.passwordValidator()]),
-    Cpassword: new FormControl("",
+    cpassword: new FormControl("",[Validators.required]
   )},{
             validators: [this.validationsService.passwordsMatchValidator] // Add custom password matching validation method from your validation service
           });
