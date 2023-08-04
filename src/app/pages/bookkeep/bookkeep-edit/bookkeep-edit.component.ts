450ce004c02f8f2f7a2481841f128cc80969ebcb
Monique <monqiquenagel24.mn@gmail.com>
2023-08-04 08:55:41 +0200

    made it so that cancel and close button properly route back to view record.

--- src/app/pages/bookkeep/bookkeep-edit/bookkeep-edit.component.ts
+++ src/app/pages/bookkeep/bookkeep-edit/bookkeep-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IncomeStatementItem } from 'src/app/models/income-statement-item';
 import { ApiService } from 'src/app/services/api/api.service';
@@ -12,16 +13,27 @@ import { BookkeepService } from 'src/app/services/bookkeep/bookkeep.service';
 export class BookkeepEditComponent implements OnInit {
     id!: any;
     record!: IncomeStatementItem;
+    editRecordForm!: FormGroup;
+    submitted = false;
+    recordType: any = ['Money In', 'Money Out'];
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
         private _bookkeepService: BookkeepService,
+        private fb: FormBuilder,
         private _apiService: ApiService
     ) {}
 
     ngOnInit() {
         this.getRecordDetails((this.id = this.route.snapshot.params['id']));
+
+        this.editRecordForm = this.fb.group({
+            recordName: ['', [Validators.required]],
+            recordType: ['', [Validators.required]],
+            recordAmount: ['', [Validators.required]],
+            recordProof: ['', [Validators.required]],
+        });
     }
 
     getRecordDetails(id: any) {
@@ -30,5 +42,32 @@ export class BookkeepEditComponent implements OnInit {
             .subscribe((data: any) => {
                 this.record = data;
             });
+    }
+
+    viewRecordDetails(recordId: any) {
+        // console.log(recordId);
+        this.router.navigate(['bookkeep/view-record', recordId]);
+    }
+
+    saveRecord() {
+        this.submitted = true;
+        if (this.editRecordForm.valid) {
+            // this.record = {
+            //     id: this._bookkeepService.generateId(),
+            //     statement_id: 0,
+            //     description: this.editRecordForm.get('recordName')?.value,
+            //     category: this.editRecordForm.get('recordType')?.value,
+            //     amount: this.editRecordForm.get('recordAmount')?.value,
+            //     proof:
+            //         'src/assets/mock-api/bookkeep-record-proof/' +
+            //         this.editRecordForm.get('recordProof')?.value,
+            // };
+            // console.table(this.record);
+            // this._apiService.addRecord(this.record).subscribe(data => {
+            //     console.log(data);
+            //     console.table(this.editRecordForm.value);
+            // });
+            // this.router.navigate(['/bookkeep']);
+        }
     }
 }
