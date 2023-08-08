98db04f40f48777c06d2ab6053cada2f8da9f963
Monique <monqiquenagel24.mn@gmail.com>
2023-08-08 08:36:26 +0200

    Created empty modal service.

--- src/app/components/modals/modal-base/modal-base.component.ts
+++ src/app/components/modals/modal-base/modal-base.component.ts
@@ -6,6 +6,7 @@ import {
     OnInit,
     OnDestroy,
 } from '@angular/core';
+import { ModalService } from 'src/app/services/modal/modal.service';
 
 @Component({
     selector: 'app-modal-base',
@@ -13,4 +14,19 @@ import {
     styleUrls: ['./modal-base.component.css'],
     encapsulation: ViewEncapsulation.None,
 })
-export class ModalBaseComponent {}
+export class ModalBaseComponent implements OnInit, OnDestroy {
+    @Input() id?: string;
+    isOpen = false;
+    private element: any;
+
+    constructor(
+        private _modalService: ModalService,
+        private el: ElementRef
+    ) {
+        this.element = el;
+    }
+
+    ngOnInit() {}
+
+    ngOnDestroy() {}
+}
