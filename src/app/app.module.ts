b25daedb4b3b366ab7acce238becf9b86cb57b29
Monique <monqiquenagel24.mn@gmail.com>
2023-08-14 07:29:20 +0200

    Created blank parent model component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -84,6 +84,7 @@ import {
     faUser,
     faUsers,
 } from '@fortawesome/free-solid-svg-icons';
+import { ModalContainerComponent } from './components/modal/modal-container/modal-container.component';
 
 @NgModule({
     declarations: [
@@ -113,6 +114,7 @@ import {
         
         EquipmentCreateComponent,
         TellMeAboutComponent,
+        ModalContainerComponent,
     ],
     imports: [
         BrowserModule,
