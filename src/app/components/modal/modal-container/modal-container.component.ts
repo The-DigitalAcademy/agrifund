3acd074f2a70f376c5d07ba1ec2eda4e8a2443b5
Monique <monqiquenagel24.mn@gmail.com>
2023-08-14 08:26:40 +0200

    Removed bootstrap javascript dependencies to properly integrate ng bootstrap.

--- src/app/components/modal/modal-container/modal-container.component.ts
+++ src/app/components/modal/modal-container/modal-container.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-modal-container',
-  templateUrl: './modal-container.component.html',
-  styleUrls: ['./modal-container.component.css']
+    selector: 'app-modal-container',
+    templateUrl: './modal-container.component.html',
+    styleUrls: ['./modal-container.component.css'],
 })
-export class ModalContainerComponent {
-
-}
+export class ModalContainerComponent {}
