bc16d39fe3742089539342d08ef8524264acb356
Monique <monqiquenagel24.mn@gmail.com>
2023-08-15 08:42:29 +0200

    Made sidebar 100% in lenght and removed styling for it on dashboard.

--- src/app/pages/bookkeep/bookkeep-view-all/bookkeep-view-all.component.spec.ts
+++ src/app/pages/bookkeep/bookkeep-view-all/bookkeep-view-all.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookkeepViewAllComponent } from './bookkeep-view-all.component';
 
 describe('BookkeepViewAllComponent', () => {
-  let component: BookkeepViewAllComponent;
-  let fixture: ComponentFixture<BookkeepViewAllComponent>;
+    let component: BookkeepViewAllComponent;
+    let fixture: ComponentFixture<BookkeepViewAllComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BookkeepViewAllComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [BookkeepViewAllComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(BookkeepViewAllComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(BookkeepViewAllComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
