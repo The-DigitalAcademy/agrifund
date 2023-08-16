da2f6dc768f142a205618f8ba04b9300aceaccf4
Monique <monqiquenagel24.mn@gmail.com>
2023-08-16 12:05:44 +0200

    Made images completely rounded. And made bootstrap carousel visible.

--- src/app/pages/get-started-page/get-started-page.component.ts
+++ src/app/pages/get-started-page/get-started-page.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+/* ------------------------------------------------------------------------------------------------
+    AUTHOR: Monique
+    CREATE DATE: 
+    UPDATED DATE: 16 Aug 2023 
+
+    DESCRIPTION:
+        This component controls the carousel component of page
+
+    PARAMETERS:
+        
+-------------------------------------------------------------------------------------------------*/
+import { Component, OnInit } from '@angular/core';
+import { NgbCarousel, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-get-started-page',
     templateUrl: './get-started-page.component.html',
     styleUrls: ['./get-started-page.component.css'],
 })
-export class GetStartedPageComponent {}
+export class GetStartedPageComponent implements OnInit {
+    // sets the first slide as the active slide
+    activeSlide = 0;
+
+    constructor(carouselConfig: NgbCarouselConfig) {}
+
+    ngOnInit() {
+        // this.pause = false;
+    }
+
+    // navigates to the next slide
+    nextSlide() {}
+
+    // navigates to the previous slide
+    previousSlide() {}
+}
