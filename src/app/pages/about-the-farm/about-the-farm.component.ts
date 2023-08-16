1011f8d073abcfbc5d7a7ba8305a70ed32b7696c
Mamokgosi <preciousmaphutha173@gmail.com>
2023-08-16 11:10:36 +0200

    added the ts component and html

--- src/app/pages/about-the-farm/about-the-farm.component.ts
+++ src/app/pages/about-the-farm/about-the-farm.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-about-the-farm',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-the-farm.component.css']
 })
 export class AboutTheFarmComponent {
+  formSlides = [
+    {
+      formGroup: this.fb.group({
+        name: ['', Validators.required],
+        // Add more form controls specific to the first slide
+      })
+    },
+    {
+      formGroup: this.fb.group({
+        age: ['', Validators.required],
+        // Add more form controls specific to the second slide
+      })
+    },
+    // Add more form slides as needed
+  ];
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(formGroup: FormGroup) {
+    if (formGroup.valid) {
+      // Handle form submission
+      const formData = formGroup.value;
+      console.log(formData);
+    }
+  }
 }
+
