117cf67c5e88641c35f315aac25ec477c07081ac
Bolebo67 <bolebo7@gmail.com>
2023-08-17 14:06:17 +0200

    I removed the convert html to pdf components

--- src/app/components/navigation/navbar/navbar.component.ts
+++ src/app/components/navigation/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AccountService } from 'src/app/account.service';
+
 
 @Component({
     selector: 'app-navbar',
