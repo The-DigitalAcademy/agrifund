0073949fe9af3993477035334b18f9bf4063244c
ntokozoblessingradebe98@gmail.com <ntokozoblessingradebe98@gmail.com>
2023-08-17 11:04:51 +0200

    added comments

--- src/app/models/users.ts
+++ src/app/models/users.ts
@@ -1,8 +1,20 @@
+// Define an interface to represent user data
 export interface Users {
+    // User's first name
     firstName: string;
+
+    // User's last name
     lastName: string;
+
+    // User's email address
     email: string;
+
+    // User's cell phone number
     cellNumber: number;
+
+    // User's password
     password: string;
+
+    // User's ID number
     idNumber: string;
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,21 +1,19 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Users } from '../models/users';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class UserService {
+    // Define a mock token for demonstration purposes
+    private mockToken = 'your_mock_token_here';
 
-private mockToken = 'your_mock_token_here';
+    constructor() {}
 
-  constructor() {}
-
-  isAuthenticated(token: string): boolean {
-    return token === this.mockToken;
-  }
+    // Check if the provided token matches the mock token
+    isAuthenticated(token: string): boolean {
+        // Compare the provided token with the mock token
+        return token === this.mockToken;
+    }
 }
-  
-
-
