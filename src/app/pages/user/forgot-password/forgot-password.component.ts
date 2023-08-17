98f7e6e3385adb49cbd4b7bca2502b923e0f8b4b
ntokozoblessingradebe98@gmail.com <ntokozoblessingradebe98@gmail.com>
2023-08-17 10:57:48 +0200

    added comments

--- src/app/pages/user/forgot-password/forgot-password.component.ts
+++ src/app/pages/user/forgot-password/forgot-password.component.ts
@@ -7,8 +7,11 @@ import { FormControl, FormGroup } from '@angular/forms';
     styleUrls: ['./forgot-password.component.css'],
 })
 export class ForgotPasswordComponent {
+    // Create a new FormGroup for the forgot password form
     ForgotPasswordForm = new FormGroup({
+        // Create a FormControl for the new password field
         Npassword: new FormControl(''),
+        // Create a FormControl for the confirm password field
         Cpassword: new FormControl(),
     });
 }
--- src/app/pages/user/reset-password/reset-password.component.ts
+++ src/app/pages/user/reset-password/reset-password.component.ts
@@ -7,7 +7,9 @@ import { FormControl, FormGroup } from '@angular/forms';
     styleUrls: ['./reset-password.component.css'],
 })
 export class ResetPasswordComponent {
+    // Create a new FormGroup for the reset password form
     ResetPasswordForm = new FormGroup({
+        // Create a FormControl for the email field
         email: new FormControl(''),
     });
 }
