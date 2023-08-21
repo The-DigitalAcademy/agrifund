6348bccf08cd902f965744fe5a52b0c3268e77b2
Monique <monqiquenagel24.mn@gmail.com>
2023-08-21 10:24:28 +0200

    Made it so that each http request has with creditials added to it.

--- src/app/_helpers/authentication/auth.interceptor.ts
+++ src/app/_helpers/authentication/auth.interceptor.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+    constructor() {}
 
-  constructor() {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
-  }
+    intercept(
+        request: HttpRequest<unknown>,
+        next: HttpHandler
+    ): Observable<HttpEvent<unknown>> {
+        request = request.clone({
+            // adds the below to each http request
+            withCredentials: true,
+        });
+        return next.handle(request);
+    }
 }
