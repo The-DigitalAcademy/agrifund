a6ca2276d0a5caf09483cba3abdd2878bd41ec47
Mamokgosi <preciousmaphutha173@gmail.com>
2023-08-21 10:22:33 +0200

    created about the farm service

--- src/app/services/aboutFarm/about-the-farm.service.ts
+++ src/app/services/aboutFarm/about-the-farm.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Crop } from 'src/app/models/crop';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AboutTheFarmService {
 
-  constructor() { }
+  private crops: Crop[] = [];  
+
+  constructor(private _apiService: ApiService) {
+
+    
+   }
+
+   // Method to generate a farm ID based on the length of 'assets' array
+  generateFarmId() {
+    const farm_id: number = this.crops.length;
+    return farm_id;
+  }
 }
