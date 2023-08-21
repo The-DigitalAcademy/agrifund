111f4641375558a680850daf557ea8c1ee364eff
Monique <monqiquenagel24.mn@gmail.com>
2023-08-21 15:12:46 +0200

    Changed api connection to secured.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    REG_URL: 'https://agrifund.tech/api/v1/auth/register/farmer',
-    LOGIN_URL: 'https://agrifund.tech/api/v1/auth/farmer',
-    API_URL: 'http://agrifund.tech/api/v1', //realApiUrl
+    // REG_URL: 'https://agrifund.tech/api/v1/auth/register/farmer',
+    // LOGIN_URL: 'https://agrifund.tech/api/v1/auth/farmer',
+    API_URL: 'https://agrifund.tech/api/v1', //realApiUrl
     mockApiUrl: 'http://localhost:3001/agrifund/api/v1', //mock api url for testing purposes
 };
