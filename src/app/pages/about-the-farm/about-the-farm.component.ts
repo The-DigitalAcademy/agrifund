150bbe0e56bdc2b4434c36c30cba9020bbe1b551
Mamokgosi <preciousmaphutha173@gmail.com>
2023-08-21 12:48:01 +0200

    Adding functionality to crop

--- src/app/pages/about-the-farm/about-the-farm.component.ts
+++ src/app/pages/about-the-farm/about-the-farm.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { NgbCarousel, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Crop } from 'src/app/models/crop';
+import { AboutTheFarmService } from 'src/app/services/aboutFarm/about-the-farm.service';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-about-the-farm',
@@ -8,72 +12,64 @@ import { NgbCarousel, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./about-the-farm.component.css']
 })
 export class AboutTheFarmComponent implements OnInit {
-  // used to refer to the bootstrap carousel on HTML
   @ViewChild('carousel', { static: true }) private carousel!: NgbCarousel;
-  // sets the first slide as the active slide
   slides: any = ['slide1', 'slide2', 'slide3', 'slide4'];
-  formSlides!: { formGroup: FormGroup }[]; // Define formSlides as an array of form group objects
+  cropForm!: FormGroup;
+  submitted = false;
+  crop!: Crop;
 
-  constructor(private fb: FormBuilder, carouselConfig: NgbCarouselConfig) {
-    // prevents the carousel from wrapping
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    carouselConfig: NgbCarouselConfig,
+    private _aboutFarm: AboutTheFarmService,
+    private _apiService: ApiService
+  ) {
     carouselConfig.wrap = false;
-    // hides dealt indicators and navigators
     carouselConfig.showNavigationArrows = false;
     carouselConfig.showNavigationIndicators = false;
-   
   }
 
   ngOnInit(): void {
-    this.formSlides= [
-      {
-        formGroup: this.fb.group({
-          // This is the welcome page for tell us about your farm
-          // Add your form controls here
-        })
-      },
-      {
-        formGroup: this.fb.group({
-          age: ['', Validators.required],
-          location: ['', Validators.required],
-          ID: ['', Validators.required],
-          size: ['', Validators.required],
-          number: ['', Validators.required],
-          funding: ['', Validators.required],
-        })
-      },
-      {
-        formGroup: this.fb.group({
-          farmLocation: ['', Validators.required],
-          farmSize: ['', Validators.required],
-          date: ['', Validators.required],
-        })
-      },
-      {
-        formGroup: this.fb.group({
-          seasonFarm: ['', Validators.required],
-          cropType: ['', Validators.required],
-          type: ['', Validators.required],
-        })
-      },
-    ];
+    this.cropForm = this.fb.group({
+      seasonFarm: ['', Validators.required],
+      cropType: ['', Validators.required],
+      type: ['', Validators.required]
+    });
     this.carousel.pause();
   }
-  // navigates to a specific slide
+
   goToSlide(item: any) {
     this.carousel.select(item);
     console.log(item);
-}
+  }
 
-// navigates to the next slide
-goToNextSlide() {
+  goToNextSlide() {
     this.carousel.next();
-}
+  }
 
-// navigates to the previous slide
-goToPreviousSlide() {
+  goToPreviousSlide() {
     this.carousel.prev();
-}
+  }
 
- 
-}
+  saveCrop() {
+    this.submitted = true;
 
+    if (this.cropForm.valid) {
+      this.crop = {
+        farm_id: this._aboutFarm.generateFarmId(),
+        season: this.cropForm.get('seasonFarm')?.value,
+        name: this.cropForm.get('cropType')?.value,
+        type: this.cropForm.get('type')?.value
+      };
+
+      console.table(this.crop);
+
+      this._apiService.addCropInfo(this.crop).subscribe(data => {
+        console.table(data);
+      });
+
+      this.router.navigate(['portfolio']);
+    }
+  }
+}
