5831783c06470d78a39c9dd565d828e487640a16
Kamogelo0404 <kamogelonkadimeng0404@gmail.com>
2023-08-23 12:20:08 +0200

    added plot info component

---
+++ src/app/components/farmer/plot-info-form/plot-info-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plot-info-form',
+  templateUrl: './plot-info-form.component.html',
+  styleUrls: ['./plot-info-form.component.css']
+})
+export class PlotInfoFormComponent {
+
+}
