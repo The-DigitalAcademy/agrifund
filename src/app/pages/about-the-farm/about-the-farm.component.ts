a511e1607d41c4858ccbb13d43e1f7cf1547bf69
Mamokgosi <preciousmaphutha173@gmail.com>
2023-08-24 11:00:35 +0200

    Fixing the UI

--- src/app/pages/about-the-farm/about-the-farm.component.ts
+++ src/app/pages/about-the-farm/about-the-farm.component.ts
@@ -16,10 +16,17 @@ import { Asset } from 'src/app/models/asset';
   styleUrls: ['./about-the-farm.component.css']
 })
 export class AboutTheFarmComponent implements OnInit {
-isDisabled: any;
-onFileSelected($event: Event) {
-throw new Error('Method not implemented.');
-}
+  isDisabled: any;
+
+  // stores the current active slide number -> default to first slide
+  activeSlideId: number = 1;
+  // stores the total slides of the carousel
+  totalSlides: number = 5;
+
+
+  onFileSelected($event: Event) {
+    throw new Error('Method not implemented.');
+  }
 
 
 
@@ -27,8 +34,8 @@ throw new Error('Method not implemented.');
   @ViewChild('carousel', { static: true }) private carousel!: NgbCarousel;
   // sets the first slide as the active slide
   slides: any = ['slide1', 'slide2', 'slide3', 'slide4'];
-  cropForm!:  FormGroup  ;
-  plotForm!:  FormGroup  ;
+  cropForm!: FormGroup;
+  plotForm!: FormGroup;
   farmForm!: FormGroup;
   equipmentForm!: FormGroup;
   submitted = false;
@@ -36,17 +43,17 @@ throw new Error('Method not implemented.');
   plot!: Plot;
   farm!: YouAndFarm;
   assets: Asset[] = [];
-isLast: any;
-assetForm!: FormGroup;
-  
+  isLast: any;
+  assetForm!: FormGroup;
 
-  constructor(private fb: FormBuilder,private router: Router, carouselConfig: NgbCarouselConfig, private _aboutFarm: AboutTheFarmService, private _apiService: ApiService) {
+
+  constructor(private fb: FormBuilder, private router: Router, carouselConfig: NgbCarouselConfig, private _aboutFarm: AboutTheFarmService, private _apiService: ApiService) {
     // prevents the carousel from wrapping
     carouselConfig.wrap = false;
     // hides dealt indicators and navigators
     carouselConfig.showNavigationArrows = false;
     carouselConfig.showNavigationIndicators = false;
-   
+
   }
 
   ngOnInit(): void {
@@ -54,103 +61,117 @@ assetForm!: FormGroup;
       seasonFarm: ['', Validators.required],
       cropType: ['', Validators.required],
       type: ['', Validators.required],
-  })
+    })
 
-  this.plotForm = this.fb.group({
-    plot_address : ['', Validators.required],
-    size: ['', Validators.required],
-    date : ['', Validators.required],
-})
-this.assetForm = this.fb.group({
-  name: ['', Validators.required],
-  type: ['', Validators.required],
-  age: ['', Validators.required],
-  purchase_price: ['', Validators.required],
-})
-this.farmForm = this.fb.group({
-  address : ['', Validators.required],
-  farm_name : ['', Validators.required],
-  farm_address:['', Validators.required],
-  years_active: ['', Validators.required],
-  num_employee : ['', Validators.required],
-  funding_reason : ['', Validators.required],
-})
-this.equipmentForm = this.fb.group({
-  equipmentName: ['', Validators.required],
-  equipmentType: ['', Validators.required],
-  purchase_Amount: ['', Validators.required],
-  age: ['', Validators.required],
-  file:['', Validators.required],
+    this.plotForm = this.fb.group({
+      plot_address: ['', Validators.required],
+      size: ['', Validators.required],
+      date: ['', Validators.required],
+    })
+    this.assetForm = this.fb.group({
+      name: ['', Validators.required],
+      type: ['', Validators.required],
+      age: ['', Validators.required],
+      purchase_price: ['', Validators.required],
+    })
+    this.farmForm = this.fb.group({
+      address: ['', Validators.required],
+      farm_name: ['', Validators.required],
+      farm_address: ['', Validators.required],
+      years_active: ['', Validators.required],
+      num_employee: ['', Validators.required],
+      funding_reason: ['', Validators.required],
+    })
+    this.equipmentForm = this.fb.group({
+      equipmentName: ['', Validators.required],
+      equipmentType: ['', Validators.required],
+      purchase_Amount: ['', Validators.required],
+      age: ['', Validators.required],
+      file: ['', Validators.required],
 
-})
+    })
     this.carousel.pause();
   }
   // navigates to a specific slide
   goToSlide(item: any) {
     this.carousel.select(item);
     console.log(item);
-}
+  }
 
-// navigates to the next slide
-goToNextSlide() {
-  this.submitted = true;
+  // navigates to the next slide
+  goToNextSlide() {
+    this.submitted = true;
 
-  if (this.cropForm.valid) {
-    this.crop = {
-      farm_id: this._aboutFarm.generateFarmId(),
-      season: this.cropForm.get('seasonFarm')?.value,
-      name: this.cropForm.get('cropType')?.value,
-      type: this.cropForm.get('type')?.value,
-    };
+    if (this.activeSlideId != this.totalSlides) {
+        // increments the active slide
+        this.activeSlideId++;
+    }
+
     
 
-    // console.table(this.crop);
+    if (this.cropForm.valid) {
+      this.crop = {
+        farm_id: this._aboutFarm.generateFarmId(),
+        season: this.cropForm.get('seasonFarm')?.value,
+        name: this.cropForm.get('cropType')?.value,
+        type: this.cropForm.get('type')?.value,
+      };
 
-    this._apiService.addCropInfo(this.crop).subscribe(data => {
-      console.table(data);
-    });
+
+      // console.table(this.crop);
+
+      this._apiService.addCropInfo(this.crop).subscribe(data => {
+        console.table(data);
+      });
+    }
+    if (this.plotForm.valid) {
+      this.plot = {
+        farm_id: this._aboutFarm.generateFarm_Id(),
+        plot_address: this.plotForm.get('plot_address')?.value,
+        size: this.plotForm.get('size')?.value,
+        ownership_date: this.plotForm.get('date')?.value,
+      };
+
+
+
+      this._apiService.addPlotInfo(this.plot).subscribe(data => {
+        console.table(data);
+      });
+    }
+
+    if (this.farmForm.valid) {
+      this.farm = {
+        farm_id: this._aboutFarm.generateFarm_FarmId(),
+        farm_name: this.farmForm.get('farm_name')?.value,
+        years_active: this.farmForm.get('years_active')?.value,
+        num_employee: this.farmForm.get('num_employee')?.value,
+        funding_reason: this.farmForm.get('funding_reason')?.value,
+      };
+
+
+
+      this._apiService.addYouandFarm(this.farm).subscribe(data => {
+        console.table(data);
+      });
+    }
+    this.carousel.next(); // Move to the next slide
+
+
   }
-  if (this.plotForm.valid) {
-    this.plot = {
-      farm_id: this._aboutFarm.generateFarm_Id(),
-      plot_address: this.plotForm.get('plot_address')?.value,
-      size: this.plotForm.get('size')?.value,
-       ownership_date: this.plotForm.get('date')?.value,
-    };
+  // navigates to the previous slide
+  goToPreviousSlide() {
 
-   
-
-    this._apiService.addPlotInfo(this.plot).subscribe(data => {
-      console.table(data);
-    });
+    if(this.activeSlideId !=1){
+      // decrements the active slide
+      this.activeSlideId--;
+    }
+      this.carousel.prev();
+  }
+  getAssetFormGroup(index: number): FormGroup {
+    // Assuming your assets array is available in this component
+    return this.assetForm;
   }
 
-  if (this.farmForm.valid) {
-    this.farm = {
-      farm_id: this._aboutFarm.generateFarm_FarmId(),
-      farm_name: this.farmForm.get('farm_name')?.value,
-      years_active: this.farmForm.get('years_active')?.value,
-      num_employee: this.farmForm.get('num_employee')?.value,
-      funding_reason: this.farmForm.get('funding_reason')?.value,
-    };
 
-   
-
-    this._apiService.addYouandFarm(this.farm).subscribe(data => {
-      console.table(data);
-    });
-  }
-  this.carousel.next(); // Move to the next slide
-
-
-}
-// navigates to the previous slide
-goToPreviousSlide() {
-    this.carousel.prev();
-}
-getAssetFormGroup(index: number): FormGroup {
-  // Assuming your assets array is available in this component
-  return this.assetForm;
-}
 
 }
