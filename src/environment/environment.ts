d48646dfd430cc7cfcf17d19d10ea09ced119d5e
Monique <Moniquenagel24.mn@gmail.com>
2023-09-04 10:40:48 +0200

    Changed api to work with localhost api for backup.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
+    // Change proxy file target to "target": "https://agrifund.tech to use deployed api and not local host",
     API_URL: '/api/v2', //completes the proxy url for the api connection
 };
