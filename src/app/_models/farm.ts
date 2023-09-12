2a32cf723aff5933a81a04cce45dc0be2cc281ef
Kamogelo0404 <kamogelonkadimeng0404@gmail.com>
2023-09-12 12:44:03 +0200

    added edit functionality

--- src/app/_models/farm.ts
+++ src/app/_models/farm.ts
@@ -1,28 +1,2 @@
-/* ------------------------------------------------------------------------------------------------
-    AUTHOR: Monique Nagel
-    CREATE DATE: 01 Sept 2023
-    UPDATED DATE: 
-
-    DESCRIPTION:
-    Model for storing the data for a Farmer's farm data
--------------------------------------------------------------------------------------------------*/
-
-import { Assets } from './Assets';
-import { Crop } from './Crop';
-import { IncomeStatement } from './IncomeStatement';
-import { Plot } from './plot';
-
-
-export interface Farm {
-    id: number;
-    numberOfEmployees: number;
-    farmName: string;
-    farmAddress: string;
-    yearsActive: number;
-    address: string; //stores residential address
-    farmingReason: string; //stores the reason for needing funding
-    crops: Crop[];
-    plots: Plot[];
-    assets: Assets[];
-    incomeStatements: IncomeStatement[];
+   incomeStatements: IncomeStatement[];
 }
