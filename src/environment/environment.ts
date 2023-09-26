b8fea5a6a50b476e7d479bbfc976362fbdb6ee4f
monaSaurusRex <moniquenagel24.mn@gmail.com>
2023-09-26 12:38:36 +0200

    App works with deployed api hosted online.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,5 +2,4 @@ export const environment = {
     production: false,
     // Change proxy file target to "target": "https://agrifund.tech to use deployed api and not local host",
     API_URL: '/api/v2', //completes the proxy url for the api connection
-    // MOCK_URL: 'http://localhost:3001/agrifund/api/v1',
 };
